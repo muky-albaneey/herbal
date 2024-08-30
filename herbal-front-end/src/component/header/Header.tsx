853bd53b918b6c1819c63482189f56c7edca64c6
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-08-30 22:49:14 +0100

    kenzy many language support

--- herbal-front-end/src/component/header/Header.tsx
+++ herbal-front-end/src/component/header/Header.tsx
@@ -52,7 +52,7 @@ function Header() {
                       <section className="left_header">
                          <NavLink to="/" className='home_btn' style={{ textDecoration: 'none' }}> {t('nav.home')}</NavLink>
                         <div className="nav_header_info">
-                        <NavLink to="/">{t('nav.product')}</NavLink>
+                        <NavLink to="/products">{t('nav.product')}</NavLink>
                         <NavLink to="/contact" className={lang_token === 'fr' ? 'french_t': ''}>{t('nav.contact_us')}</NavLink>
                         </div>
                       </section>
@@ -67,8 +67,8 @@ function Header() {
         <input 
             type="search" 
             id="default-search" 
-            className="block w-full p-3 ps-10 text-sm text-gray-900 border border-green-300 rounded-lg bg-green-50 focus:ring-green-500 focus:border-blue-500 dark:bg-green-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            placeholder="Search Mockups, Logos..." 
+            className="block w-full p-3 ps-10 text-sm text-white-900 border border-green-300 rounded-lg bg-green-50 focus:ring-green-500 focus:border-blue-500 dark:bg-green-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Search for anything you want" 
             required 
         />
         <button 
@@ -76,13 +76,9 @@ function Header() {
             className="text-white absolute end-2.5 bottom-2.5 bg-green-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 dark:bg-green-600 dark:hover:bg-blue-700 dark:focus:ring-green-800" id='d-s-btn'>
             Search
         </button>
-    </div>
-</form>
+           </div>
+          </form>
 
-                      {/* <form action="" method="post" className="d-flex justify-center items-center relative">
-                          <BiSearchAlt2 className='serch_icon absolute lg:left-0 ' id="search_mobile"/>
-                          <input type="search"  placeholder={t('search_placeholder')} id='search_input' />
-                      </form> */}
                       </section>
                       <section className="right_header">
                       <NavLink to="fan" className='cart_header_btn' style={{ textDecoration: 'none', display:'flex' }}> <ImCart />{t('nav.cart')}</NavLink>
--- herbal-front-end/src/pages/product/Products.tsx
+++ herbal-front-end/src/pages/product/Products.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ListComponent_info from '../../component/products/Product_list';
+import SearchWithButtonExample from '../../component/Search';
 
 
 export default function ProductsComponent() {
@@ -7,6 +8,10 @@ export default function ProductsComponent() {
     <div className='productListWrapper'>
     <aside className="filter_sec">
       <h1>PRODUCTS</h1>
+      <div className="search_product">
+        <SearchWithButtonExample />
+      </div>
+      
     </aside>
     <main className="house_listing">
      <ListComponent_info />
