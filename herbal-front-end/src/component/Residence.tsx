f91c2ee7306b3265358e985da297354d73d74494
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-22 11:57:50 +0100

    perfecting cart

--- herbal-front-end/src/component/Residence.tsx
+++ herbal-front-end/src/component/Residence.tsx
@@ -9,7 +9,41 @@ import { Card } from 'antd';
 import { Link } from 'react-router-dom';
 
 const SliderButtons = ({side}) => {
-    const swiper = useSwiper(); 
+
+
+const [products, setProducts] = React.useState<Product[]>([]);
+const [loading, setLoading] = React.useState(true);
+const [error, setError] = React.useState<string | null>(null);
+  
+React.useEffect(() => {
+      const fetchProducts = async () => {
+        try {
+          const response = await axios.get('https://backend-herbal.onrender.com/products/all', {
+            withCredentials: true,
+            headers: {
+              'Cache-Control': 'no-cache', // Prevent caching of the response
+            },
+          });
+          setProducts(response.data);
+        } catch (err) {
+          setError('Error fetching products');
+          console.error(err);
+        } finally {
+          setLoading(false);
+        }
+      };
+  
+      fetchProducts();
+    }, []);
+  
+    if (loading) return <div>Loading products...</div>;
+    if (error) return <div>{error}</div>;
+  
+    // Check if there are no products available
+    if (products.length === 0) {
+      return <div>No products available</div>;
+    }
+const swiper = useSwiper(); 
     return (
         <div className={side == true ? 'r-button' : 'r-buttons'}>
             <button onClick={() => swiper.slidePrev()} >&lt;</button>
