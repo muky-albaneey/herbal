b4b374f4d6985135af37752dcdb350685a2cb1eb
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-29 07:41:09 +0100

    adding outlet to the user

--- herbal-front-end/src/pages/auth/UserLayout.tsx
+++ herbal-front-end/src/pages/auth/UserLayout.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 export default function Users() {
   return (
-    <>
-    
-    </>
+    <div>
+        <section className="users_left">
+
+        </section>
+        <section className="user_right">
+
+        <Outlet />
+        </section>
+    </div>
   );
 }
