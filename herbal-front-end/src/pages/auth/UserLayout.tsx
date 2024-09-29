60a1f0e98d2228014065fc99ec003c7370bc1912
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-29 10:44:18 +0100

    stylin user layout

--- herbal-front-end/src/pages/auth/UserLayout.tsx
+++ herbal-front-end/src/pages/auth/UserLayout.tsx
@@ -1,51 +1,29 @@
-// import React from 'react';
-// import { NavLink, Outlet } from 'react-router-dom';
-
-// export default function Users() {
-//   const active = {
-//     backgroundColor: '#008103',
-//     color: '#fff',
-//     textDecoration: 'underline',
-//     fontWeight:'bold'
-// }
-
-//   return (
-//     <div>
-//         <section className="users_left">
-//         <NavLink style={({isActive}) => isActive ? active :  {color: '#FFFFFF'}} className='f_link' to='.' end >Fixtures</NavLink>
-//         <NavLink style={({isActive}) => isActive ? active : {color: '#FFFFFF'}} className='t_link' to='profile'>Table</NavLink>
-//         </section>
-//         <section className="user_right">
-
-//         <Outlet />
-//         </section>
-//     </div>
-//   );
-// }
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 
 export default function Users() {
   const active = {
-    // width:'10rem',
+    // width:'14rem',
     backgroundColor: '#008103',
     color: '#fff',
-    textDecoration: 'underline',
-    fontWeight: 'bold',
-    padding: '1.5rem',
+    // textDecoration: 'underline',
+    // fontWeight: 'bold',
+    // padding: '.5rem',
   };
 
   return (
-    <div>
+    <div className='users_wrapper'>
       <div className="users_header">
         <h5>Profile</h5>
         <p>Edit and enable your profile setting </p>
       </div>
+      <div className="users_content">
       <section className="users_left">
-        {/* NavLink for 'Fixtures' pointing to the index route */}
+        <div className="users_con">
+          {/* NavLink for 'Fixtures' pointing to the index route */}
         <NavLink 
           style={({ isActive }) => (isActive ? active : { color: '#008103' })} 
-          className='f_link' 
+          className='user_link' 
           to='.' 
           end
         >
@@ -55,17 +33,19 @@ export default function Users() {
         {/* NavLink for 'Table' pointing to 'profile' sub-route */}
         <NavLink 
           style={({ isActive }) => (isActive ? active : { color: '#008103' })} 
-          className='t_link' 
+          className='user_link' 
           to='profile'
         >
           Change password
         </NavLink>
+        </div>
       </section>
 
       <section className="user_right">
         {/* Display the content of the selected route here */}
         <Outlet />
       </section>
+      </div>
     </div>
   );
 }
