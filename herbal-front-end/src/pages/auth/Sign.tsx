1fe4db5b3ac67b773606015a004eb0cf4825b0b3
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-29 08:54:27 +0100

    adding outlet to the user

--- herbal-front-end/src/pages/auth/Sign.tsx
+++ herbal-front-end/src/pages/auth/Sign.tsx
@@ -327,7 +327,7 @@ const SignUpForm = () => {
           disabled={isSubmitting}
           className="py-4 bg-green-500 hover:bg-green-600 text-white rounded-lg w-full"
         >
-          {isSubmitting ? 'Submitting...' : 'Submit'}
+          {isSubmitting ? 'LogIng...' : 'LogIn'}
         </button>
       </form>
 
