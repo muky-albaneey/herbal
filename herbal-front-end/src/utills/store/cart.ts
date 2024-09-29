99ae7469b8b35b3902ebca77b4cf33416af1cb54
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-29 12:53:39 +0100

    adding sticky to the header

--- herbal-front-end/src/utills/store/cart.ts
+++ herbal-front-end/src/utills/store/cart.ts
@@ -1,3 +1,62 @@
+// // export default useCartStore;
+// import { create } from 'zustand';
+
+// interface CartItem {
+//   id: string;
+//   name: string;
+//   price: number;
+//   quantity: number;
+// }
+
+// interface CartStore {
+//   cart: CartItem[];
+//   addToCart: (item: CartItem) => void;
+//   removeFromCart: (id: string) => void;
+//   totalPrice: () => number;
+// }
+
+// const useCartStore = create<CartStore>((set, get) => ({
+//   cart: [],
+//   addToCart: (item) =>
+//     set((state) => {
+//       const existingItemIndex = state.cart.findIndex((cartItem) => cartItem.id === item.id);
+//       let updatedCart;
+
+//       if (existingItemIndex >= 0) {
+//         // Item already exists in the cart, update its quantity
+//         const updatedItem = {
+//           ...state.cart[existingItemIndex],
+//           quantity: state.cart[existingItemIndex].quantity + item.quantity,
+//         };
+//         updatedCart = state.cart.map((cartItem, index) =>
+//           index === existingItemIndex ? updatedItem : cartItem
+//         );
+//       } else {
+//         // Item does not exist, add it to the cart
+//         updatedCart = [...state.cart, item];
+//       }
+
+//       // Remove any items with zero quantity
+//       const filteredCart = updatedCart.filter((cartItem) => cartItem.quantity > 0);
+      
+//       return { cart: filteredCart };
+//     }),
+
+//   removeFromCart: (id) =>
+//     set((state) => {
+//       console.log('Removing item with id:', id); // Debugging to check the id being passed
+//       const updatedCart = state.cart.filter((item) => item.id !== id);
+//       console.log('Updated cart after removal:', updatedCart); // For debugging after the cart is updated
+//       return { cart: updatedCart };
+//     }),
+
+//   totalPrice: () => {
+//     const { cart } = get(); // Access the cart state
+//     return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+//   },
+// }));
+
+// export default useCartStore;
 import { create } from 'zustand';
 
 interface CartItem {
@@ -16,8 +75,7 @@ interface CartStore {
 }
 
 const useCartStore = create<CartStore>((set, get) => ({
-  // cart: [],
-  cart: JSON.parse(localStorage.getItem('cart') || '[]'),
+  cart: [],
   addToCart: (item) =>
     set((state) => {
       const existingItemIndex = state.cart.findIndex((cartItem) => cartItem.id === item.id);
@@ -36,8 +94,7 @@ const useCartStore = create<CartStore>((set, get) => ({
       }
 
       const filteredCart = updatedCart.filter((cartItem) => cartItem.quantity > 0);
-      // Save the updated cart to localStorage
-      localStorage.setItem('cart', JSON.stringify(filteredCart));
+      
       return { cart: filteredCart };
     }),
 
