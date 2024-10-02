9fd08c86d8b51292ad54ac13b8959e38a3e394c9
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-10-02 19:39:41 +0100

    adding auto card

--- herbal-front-end/src/component/AutoCard.tsx
+++ herbal-front-end/src/component/AutoCard.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function AutoPlay() {
   const settings = {
