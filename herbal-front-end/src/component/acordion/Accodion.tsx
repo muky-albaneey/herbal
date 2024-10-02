1965a875aa5924e10d4009aeeeefb375b3864cf0
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-10-02 14:38:36 +0100

    editing verifying payment page

--- herbal-front-end/src/component/acordion/Accodion.tsx
+++ herbal-front-end/src/component/acordion/Accodion.tsx
@@ -21,35 +21,38 @@ const CustomCollapse = ({ label, children }) => {
 
 export default function AccodionComponent() {
   const { t } = useTranslation();
-  const text = t('text');
+  const text1 = t('text');
+  const text2 = t('text');
+  const text3 = t('text');
+  const text4 = t('text');
 
   return (
     <div className='acodion_wrapper'>
       <div className="collapse_item">
-        <CustomCollapse label={t('collapse.label')}>
-          <p>{text}</p>
+        <CustomCollapse label={t('collapse1.label')}>
+          <p>{text1}</p>
         </CustomCollapse>
       </div>
       <div className="collapse_item">
-        <CustomCollapse label={t('collapse.label')}>
-          <p>{text}</p>
+        <CustomCollapse label={t('collapse2.label')}>
+          <p>{text2}</p>
         </CustomCollapse>
       </div>
       <div className="collapse_item">
-        <CustomCollapse label={t('collapse.label')}>
-          <p>{text}</p>
+        <CustomCollapse label={t('collapse3.label')}>
+          <p>{text3}</p>
         </CustomCollapse>
       </div>
       <div className="collapse_item">
+        <CustomCollapse label={t('collapse4.label')}>
+          <p>{text4}</p>
+        </CustomCollapse>
+      </div>
+      {/* <div className="collapse_item">
         <CustomCollapse label={t('collapse.label')}>
           <p>{text}</p>
         </CustomCollapse>
-      </div>
-      <div className="collapse_item">
-        <CustomCollapse label={t('collapse.label')}>
-          <p>{text}</p>
-        </CustomCollapse>
-      </div>
+      </div> */}
     </div>
   );
 }
