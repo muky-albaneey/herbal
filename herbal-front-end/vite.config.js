26189d10b4a5ef76514b2682d36676db78c6deba
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-10-02 09:18:22 +0100

    editing verifying payment page

--- herbal-front-end/vite.config.js
+++ herbal-front-end/vite.config.js
@@ -1,3 +1,32 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src'),
+//     },
+//   },
+//   build: {
+//     outDir: 'dist', // Output in the 'dist' folder
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           // Create separate chunks for node_modules
+//           if (id.includes('node_modules')) {
+//             return id.split('node_modules/')[1].split('/')[0].toString();
+//           }
+//         },
+//         chunkSizeWarningLimit: 500, // Set custom chunk size limit for warnings
+//       },
+//     },
+//   },
+//   optimizeDeps: {
+//     exclude: ['react-icons'],
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
