82aa42ca1caff2dd7fb5a9ce1045c7b685ebba8c
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-10-03 12:48:03 +0100

    adding auto card

--- herbal-front-end/src/component/Testimony.tsx
+++ herbal-front-end/src/component/Testimony.tsx
@@ -1,102 +1,3 @@
-// import React from 'react';
-// import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
-// import 'swiper/css';
-// import './residence.css';
-// import { sliderSettings } from '../utills/common';
-// import { Card } from 'antd';
-// import { Link } from 'react-router-dom';
-// import axios from 'axios';
-
-// export type ProductImage = {
-//   id: string;
-//   name: string;
-//   url: string;
-//   ext: string;
-// };
-
-// export type User = {
-//   id: string;
-//   full_name: string;
-//   email: string;
-//   phone_num: string | null;
-//   password: string;
-//   location: string | null;
-//   rememberToken: string | null;
-//   role: string;
-//   profile_image: string | null;
-// };
-
-// export type Product = {
-//   id: string;
-//   name: string;
-//   price: string;  // Price is a string in your response
-//   quantity: string;  // Quantity is a string in your response
-//   category: string;
-//   description: string;
-//   product_image: ProductImage;  // Nested object for product image details
-//   user: User;  // Nested object for user details
-//   createdAt: string;  // ISO date string
-// };
-
-
-// const products =[
-//   './testimony.jpg',
-//   './testimony2.jpg',
-//   './testimony3.jpg',
-//   './testimony4.jpg'
-// ]
-
-// const TestimonyComponent = React.memo(() => {
-
-//   const SliderButtons = () => {
-    
-//     const swiper = useSwiper(); 
-//         return (
-//             <div className='r-buttons'>
-//                 <button onClick={() => swiper.slidePrev()} >&lt;</button>
-//                 <button onClick={() => swiper.slideNext()}>&gt;</button>
-//             </div>
-//         );
-//     };
-
-//     return (
-//         <section className="r-wrapper">
-//             <div className="r-container">
-//                 <Swiper {...sliderSettings}>
-//                     {products.map((card, i) => (
-//                         <SwiperSlide key={i}>
-//                            <Link to='#' style={{ textDecoration:'none' }}>
-//                            <div className="flexColStart r-card">
-//                                 <Card
-//                                     hoverable
-//                                     className='cardCon'
-//                                     id='cardConImg'
-//                                     cover={
-                                   
-//                                         <img 
-//                                          className="cardConImg"
-//                                         src={card} 
-//                                         alt={`Testimony ${i + 1}`}
-//                                         loading="lazy" 
-                                        
-//                                       />
-
-//                                   }
-//                                 >
-                                   
-//                                 </Card>
-//                             </div>
-//                            </Link>
-//                         </SwiperSlide>
-//                     ))}
-//                     <SliderButtons /> 
-//                 </Swiper>
-//             </div>
-//         </section>
-//     );
-// });
-
-// export default TestimonyComponent;
 import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -115,7 +16,7 @@ function AutoPlay() {
   const settings = {
     dots: true,
     infinite: true,
-    slidesToShow: 1, // Show 1 image on mobile
+    slidesToShow: 4, // Show 1 image on mobile
     slidesToScroll: 1,
     autoplay: true,
     speed: 1000, // Transition speed
