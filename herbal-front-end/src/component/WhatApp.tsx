9ff106c21bba8ce220427c93352c1edee0ca5389
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-10-05 09:14:07 +0100

    Adding whatsApp

--- herbal-front-end/src/component/WhatApp.tsx
+++ herbal-front-end/src/component/WhatApp.tsx
@@ -1,32 +1,23 @@
-import { BiGasPump } from "react-icons/bi"; 
-import { RiGasStationFill } from "react-icons/ri"; 
 import React from 'react';
-// import './whatsapp.css';
 import { WhatsAppWidget } from 'react-whatsapp-widget';
 import 'react-whatsapp-widget/dist/index.css';
 import logo from '../assets/logo.png';
 
 const Whatsapp = () => {
-  return (     
-    
-    
-      <div className="whatsapp-widget-container" > 
-      
-      {/* WhatsApp Widget */}
-        <WhatsAppWidget 
-      CompanyIcon={logo}
-      companyName ='Kenzy Naturals.' 
-      phoneNumber="7060941707"
-      // containerClass="whatsapp-widget-container"      
-      sendButtonText = 'chart us'
-      inputPlaceHolder = 'say hi!'
-      replyTimeText = 'we reply immediately after your message'      
-      open = {false}
-      style={{marginTop: '2rem'}}
-    />
+  return (    
+    <div className="whatsapp-widget-container"> 
+      <WhatsAppWidget 
+        CompanyIcon={logo}
+        companyName='Kenzy Naturals'
+        phoneNumber="7060941707"
+        sendButtonText='Chat us'
+        inputPlaceHolder='Say hi!'
+        replyTimeText='We reply immediately after your message'      
+        open={false}
+        style={{ marginTop: '2rem' }}
+      />
     </div>
-    
   )
 }
 
-export default Whatsapp
+export default Whatsapp;
