f469556b888c6f978ef9eac478cfb926988f77bc
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-10-05 09:32:24 +0100

    Adding whatsApp

--- herbal-front-end/src/component/WhatApp.tsx
+++ herbal-front-end/src/component/WhatApp.tsx
@@ -1,29 +1,54 @@
+// import { BiGasPump } from "react-icons/bi"; 
+// import { RiGasStationFill } from "react-icons/ri"; 
+// import React from 'react';
+// // import './whatsapp.css';
+// import { WhatsAppWidget } from 'react-whatsapp-widget';
+// import 'react-whatsapp-widget/dist/index.css';
+// // import logo from '../assets/logo.png';
+
+// const Whatsapp = () => {
+//   return (     
+    
+    
+//     <div className="whatsapp-widget-container"> 
+//     <WhatsAppWidget 
+//       CompanyIcon={'/logo.png'}
+//       companyName='Kenzy Naturals'
+//       phoneNumber="7060941707"
+//       sendButtonText='Chat us'
+//       inputPlaceHolder='Say hi!'
+//       replyTimeText='We reply immediately after your message'      
+//       open={false}
+//       style={{ marginTop: '2rem' }}
+//     />
+//   </div>
+    
+//   )
+// }
+
+// export default Whatsapp
 import { BiGasPump } from "react-icons/bi"; 
 import { RiGasStationFill } from "react-icons/ri"; 
 import React from 'react';
-// import './whatsapp.css';
 import { WhatsAppWidget } from 'react-whatsapp-widget';
 import 'react-whatsapp-widget/dist/index.css';
-// import logo from '../assets/logo.png';
+import logo from '../assets/logo.png';
 
 const Whatsapp = () => {
-  return (     
-    
-    
-    <div className="whatsapp-widget-container"> 
-    <WhatsAppWidget 
-      CompanyIcon={'/logo.png'}
-      companyName='Kenzy Naturals'
-      phoneNumber="7060941707"
-      sendButtonText='Chat us'
-      inputPlaceHolder='Say hi!'
-      replyTimeText='We reply immediately after your message'      
-      open={false}
-      style={{ marginTop: '2rem' }}
-    />
-  </div>
-    
-  )
+  return (
+    <div className="whatsapp-widget-container">
+      <WhatsAppWidget 
+        CompanyIcon={logo} // Pass the imported logo here
+        companyName='Kenzy Naturals'
+        phoneNumber="7060941707"
+        sendButtonText='Chat us'
+        inputPlaceHolder='Say hi!'
+        replyTimeText='We reply immediately after your message'      
+        open={false}
+        style={{ marginTop: '2rem' }}
+      />
+    </div>
+  );
 }
 
-export default Whatsapp
+export default Whatsapp;
