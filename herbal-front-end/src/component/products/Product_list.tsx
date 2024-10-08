485423d495af472c11572f297a6718743bc0d492
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-10-08 08:41:12 +0100

    edditing product list page

--- herbal-front-end/src/component/products/Product_list.tsx
+++ herbal-front-end/src/component/products/Product_list.tsx
@@ -71,7 +71,32 @@ const ListComponent_info = React.memo(({ category }) => {
                         </Link>
                     ))
                 ) : (
-                    <p>No products found</p>
+                    <>
+                        <div className="spinner-grow text-primary" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-secondary" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-success" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-danger" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-warning" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-info" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-light" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-dark" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                    </>
                 )}
             </div>
         </section>
