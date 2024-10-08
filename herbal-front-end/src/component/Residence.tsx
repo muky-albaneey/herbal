b635c2841d1e4328d169ba0787fb66a49364b668
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-10-08 10:13:56 +0100

    edditing product list page

--- herbal-front-end/src/component/Residence.tsx
+++ herbal-front-end/src/component/Residence.tsx
@@ -295,7 +295,32 @@ const Residence = React.memo(({ message }) => {
 
     // Check if there are no products available
     if (products.length === 0) {
-      return <div>No products available</div>;
+      return <div>
+          <div className="spinner-grow text-primary" role="status">
+      <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-grow text-secondary" role="status">
+      <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-grow text-success" role="status">
+      <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-grow text-danger" role="status">
+      <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-grow text-warning" role="status">
+      <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-grow text-info" role="status">
+      <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-grow text-light" role="status">
+      <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-grow text-dark" role="status">
+      <span className="visually-hidden">Loading...</span>
+  </div>
+      </div>;
     }
 
     return (
